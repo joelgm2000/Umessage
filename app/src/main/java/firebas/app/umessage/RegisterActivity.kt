a6977465e0517bb1d1b1561c76a07cba908970ad
joelgm2000 <joelgarciamaestrejgm@gmail.com>
2020-11-19 17:08:42 -0500

    4

---
+++ app/src/main/java/firebas/app/umessage/RegisterActivity.kt
@@ -0,0 +1,29 @@
+package firebas.app.umessage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_register)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Register"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this@RegisterActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+
+    }
+}
